a09214198ba7ab31bfe2ffc2ad3656715a155539
Diego Santana <3c.santana@gmail.com>
2024-04-15 08:33:22 -0300

    refactor: modifica o método insert para retornar um PersonDTO

--- src/main/java/com/devsuperior/aula/services/PersonService.java
+++ src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.aula.services;
 
+import com.devsuperior.aula.dto.PersonDTO;
 import com.devsuperior.aula.dto.PersonDepartmentDTO;
 import com.devsuperior.aula.entities.Department;
 import com.devsuperior.aula.entities.Person;
@@ -16,13 +17,13 @@ public class PersonService {
     @Autowired
     private DepartmentRepository departmentRepository;
     
-    public PersonDepartmentDTO insert(PersonDepartmentDTO dto) {
+    public PersonDTO insert(PersonDTO dto) {
         Person person = new Person();
         person.setName(dto.getName());
         person.setSalary(dto.getSalary());
-        Department department = departmentRepository.getReferenceById(dto.getDepartment().getId());
+        Department department = departmentRepository.getReferenceById(dto.getDepartmentId());
         person.setDepartment(department);
         person = personRepository.save(person);
-        return new PersonDepartmentDTO(person);
+        return new PersonDTO(person);
     }
 }
