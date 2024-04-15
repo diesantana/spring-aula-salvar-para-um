edd598ed5a30f922887602da40b89f3812b94d83
Diego Santana <3c.santana@gmail.com>
2024-04-14 23:47:29 -0300

    feat: cria o PersonController

---
+++ src/main/java/com/devsuperior/aula/controllers/PersonController.java
@@ -0,0 +1,33 @@
+package com.devsuperior.aula.controllers;
+
+
+import com.devsuperior.aula.dto.PersonDepartmentDTO;
+import com.devsuperior.aula.entities.Department;
+import com.devsuperior.aula.entities.Person;
+import com.devsuperior.aula.services.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/people")
+public class PersonController {
+    
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping
+    public ResponseEntity<PersonDepartmentDTO> insert(@RequestBody  PersonDepartmentDTO dto) {
+        PersonDepartmentDTO response = personService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(response.getId()).toUri();
+        return ResponseEntity.created(uri).body(response);
+    }
+    
+}
